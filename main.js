f99e679cda3756bdb97b36baa38845277d521a08
Jose <josemanuel21302@gmail.com>
2023-02-22 19:43:28 -0800

    able to get other buisses

--- main.js
+++ main.js
@@ -22,4 +22,45 @@ if (navigator.geolocation) {
       }).addTo(map);
   }
 
-//I will make buttons where it would select coffees
+// get foursquare businesses
+async function getFoursquare(business) {
+  const options = {
+  method: 'GET',
+  headers: {
+  Accept: 'application/json',
+  Authorization: 'fsq3ATzZbmcGhdeFafr73wZcnJ+LlN6bK+4dh19a7ClS4u8='
+  }
+  }
+  let limit = 4
+  let position = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  })
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  
+  let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+  let data = await response.text()
+  let parsedData = JSON.parse(data)
+  
+  let businesses = parsedData.results
+  return businesses
+  }
+
+
+
+//search button
+async function searchLocation () {
+  let selectedLocation = document.getElementById(`Locations`).value // get the value of the drop down
+  console.log(selectedLocation)
+  
+  let businesses = await getFoursquare(selectedLocation)
+  console.log(businesses);
+  businesses.forEach(function(business) {
+    addMarker(business.latitude, business.longitude);
+  });
+}
+
+
+function addMarker(lat, lng) { // this will add new markers to the map
+  var marker = L.marker([lat, lng]).addTo(map);
+}
